fd485fe47c46dd812ff99aa5516beb4b0451f2e9
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-05 01:54:20 -0700

    Time: 72 ms (86.58%), Space: 17.3 MB (74.69%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -14,17 +14,33 @@ class Solution:
             dic[words[i]] += 1
         # print(dic)
         res = []
-        for i in range(str_len - substr_len+1):
+        for j in range(word_len):
+            # print(j, str_len - substr_len - word_len + 1)
             dic1 = {}
-            for j in range(i, i + substr_len, word_len):
-                word = s[j:j+word_len]
+            for k in range(j, j + substr_len, word_len):
+                word = s[k:k+word_len]
                 # print(word)
-                if word not in dic:
-                    break
                 if word not in dic1:
                     dic1[word] = 0
                 dic1[word] += 1
-            # print(dic1)
             if dic == dic1:
-                res.append(i)
+                res.append(j)
+            # print(dic1, res)
+            for i in range(j, str_len - substr_len - word_len + 1, word_len):
+                word_removed = s[i:i+word_len]
+                word_added = s[i+substr_len:i+substr_len+word_len]
+                # print(word_removed, word_added)
+                if word_removed in dic1:
+                    if dic1[word_removed] == 1:
+                        dic1.pop(word_removed)
+                    else:
+                        dic1[word_removed] -= 1
+                else:
+                    break
+                if word_added not in dic1:
+                    dic1[word_added] = 0
+                dic1[word_added] += 1
+                # print(dic1)
+                if dic == dic1:
+                    res.append(i+word_len)
         return res
