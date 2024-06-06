86f1a49a212fb00e9db2950b949d97a5026cb93d
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-05 19:36:21 -0700

    Time: 160 ms (67.72%), Space: 18.5 MB (50.76%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -3,18 +3,16 @@ class Solution:
         n = len(hand)
         if n%groupSize != 0:
             return False
-        dic = {}
-        for num in hand:
-            if num not in dic:
-                dic[num] = 0
-            dic[num] += 1
-        for _ in range(0, n, groupSize):
-            start = min(dic)
-            for j in range(groupSize):
-                if start+j not in dic:
-                    return False
-                if dic[start+j] == 1:
-                    dic.pop(start+j)
-                else:
-                    dic[start+j] -= 1
+        dic = Counter(hand)
+        sorted_keys = sorted(dic.keys())
+        # print(dic, sorted_keys)
+        for i in sorted_keys:
+            count = dic[i]
+            if count > 0:
+                for j in range(groupSize):
+                    # print(i, i+j, count, dic[i+j])
+                    if dic[i+j] < count:
+                        return False
+                    dic[i+j] -= count
+            # print(dic)
         return True
