fea4f7cc250a6b3700131daffa4da828c4d98d63
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-07 04:11:53 -0700

    Time: 84 ms (90.72%), Space: 16.6 MB (55.83%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,21 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        dic = {}
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
         for row in range(9):
             for col in range(9):
                 if board[row][col] == '.':
                     continue
                 # print(row, col, board[row][col], dic)
-                if (('row_' + str(row)) in dic and board[row][col] in dic['row_' + str(row)]) or (('col_' + str(col)) in dic and board[row][col] in dic['col_' + str(col)]) or (('box_' + str(3 * (row//3) + col//3)) in dic and board[row][col] in dic['box_' + str(3 * (row//3) + col//3)]):
+                if (board[row][col] in rows[row]) or (board[row][col] in cols[col]) or  (board[row][col] in boxes[str(3 * (row//3) + col//3)]):
                     return False
-                if ('row_' + str(row)) not in dic:
-                    dic['row_' + str(row)] = set()
-                if ('col_' + str(col)) not in dic:
-                    dic['col_' + str(col)] = set()
-                if ('box_' + str(3 * (row//3) + col//3)) not in dic:
-                    dic['box_' + str(3 * (row//3) + col//3)] = set()
-                dic['row_' + str(row)].add(board[row][col])
-                dic['col_' + str(col)].add(board[row][col])
-                dic['box_' + str(3 * (row//3) + col//3)].add(board[row][col])
+                rows[row].add(board[row][col])
+                cols[col].add(board[row][col])
+                boxes[str(3 * (row//3) + col//3)].add(board[row][col])
         return True
 
