c05d0c57d0df27e1be65452081dde86cb03c7968
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-07 02:04:00 -0700

    Time: 29 ms (93.21%), Space: 16.4 MB (73.46%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,25 +1,20 @@
 class Solution:
-    def getDigits(self, n):
-        l = []
+    def getSumOfSquaresOfDigits(self, n):
+        sum_of_squares_of_digits = 0
         while n > 0:
-            l.append(n%10)
+            sum_of_squares_of_digits += (n%10) ** 2
             n = n//10
-        return tuple(sorted(l))
+        return sum_of_squares_of_digits
 
     def isHappy(self, n: int) -> bool:
         dic = set()
         if n == 1:
             return True
         while True:
-            digits = self.getDigits(n)
-            print(digits)
-            if digits in dic:
-                return False
-            sum_of_squares_of_digits = 0
-            for i in digits:
-                sum_of_squares_of_digits += i*i
-            if sum_of_squares_of_digits == 1:
+            n = self.getSumOfSquaresOfDigits(n)
+            if n == 1:
                 return True
-            dic.add(digits)
-            n = sum_of_squares_of_digits
+            if n in dic:
+                return False
+            dic.add(n)
             
