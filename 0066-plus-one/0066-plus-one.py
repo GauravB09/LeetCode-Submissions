8dea870ecc3aee5c009d4c871ed034b47472ab19
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-08 03:11:19 -0700

    Time: 47 ms (5.22%), Space: 16.4 MB (77.81%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         n = len(digits)
-        if n == 1:
-            if digits[0] == 9:
-                return [1, 0]
-            return [digits[0]+1]
         for i in range(n-1, -1, -1):
-            if digits[i]!=9:
+            if digits[i]==9:
+                digits[i] = 0
+            else:
                 digits[i] += 1
-                break
-            digits[i] = 0
-        if i == 0 and digits[0] == 0:
-            return [1] + digits
-        return digits
+                return digits
+        return [1] + digits
