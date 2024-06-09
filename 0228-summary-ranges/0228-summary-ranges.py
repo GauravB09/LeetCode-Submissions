a8a58c77e18ed20762e3e1c0bbeb5c4df995d28e
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-09 04:49:39 -0700

    Time: 41 ms (13.51%), Space: 16.5 MB (38.26%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,17 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        res = []
+        nums = nums + [(-1) * ((2 ** 31) + 1)]
+        n = len(nums)
+        start = nums[0]
+        for i in range(n-1):
+            if nums[i]+1 != nums[i+1]:
+                if start != nums[i]:
+                    res.append(str(start) + "->" + str(nums[i]))
+                else:
+                    res.append(str(start))
+                start = nums[i+1]
+        print(res)
+        return res
+
+            
