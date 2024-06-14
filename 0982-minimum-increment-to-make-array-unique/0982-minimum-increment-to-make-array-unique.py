ce31e62d7fe4a83f654f02337640503d9c0101a3
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-14 11:15:34 -0700

    Time: 590 ms (89.41%), Space: 30.3 MB (66.44%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
@@ -1,24 +1,40 @@
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
+        # ans = 0
+        # max_num = max(nums) + 1
+        # arr = [0 for _ in range(max_num)]
+        # for num in nums:
+        #     arr[num] += 1
+        # s = []
+        # length = 0
+        # # print(arr)
+        # for i in range(max_num ):
+        #     # print(s, arr[i], length)
+        #     if arr[i] == 0 and length > 0:
+        #         ans += i - s.pop()
+        #         length -= 1
+        #     elif arr[i] > 1:
+        #         s = [i for _ in range(arr[i] - 1)] + s
+        #         length += (arr[i] - 1)
+        # # print(s)
+        # for i in s:
+        #     ans += max_num - i
+        #     max_num += 1
+        # return ans
+
         ans = 0
         max_num = max(nums) + 1
         arr = [0 for _ in range(max_num)]
         for num in nums:
             arr[num] += 1
-        s = []
-        length = 0
         # print(arr)
-        for i in range(max_num ):
-            # print(s, arr[i], length)
-            if arr[i] == 0 and length > 0:
-                ans += i - s.pop()
-                length -= 1
-            elif arr[i] > 1:
-                s = [i for _ in range(arr[i] - 1)] + s
-                length += (arr[i] - 1)
-        # print(s)
-        for i in s:
-            ans += max_num - i
-            max_num += 1
+        for i in range(max_num - 1):
+            if arr[i] > 0:
+                temp = arr[i] - 1
+                ans += temp
+                arr[i + 1] += temp
+        # print(arr)
+        # print(arr[i])
+        ans += arr[max_num-1] * (arr[max_num-1] - 1)//2
         return ans
             
