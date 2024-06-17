223c5fb467d92d91b9f08086b30759fe93c88f09
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-17 01:15:45 -0700

    Time: 1611 ms (5%), Space: 16.4 MB (79.87%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,14 +1,27 @@
 class Solution:
+    def isSquare(self, n: int) -> bool:
+        if n <= 1:
+            return True
+        left, right = 1, n
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+            if square == n:
+                return True
+            elif square < n:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
     def judgeSquareSum(self, c: int) -> bool:
-        res = [0]
+        if self.isSquare(c):
+            return True
         temp = 1
         num = 0
         while num <= c:
             num = temp * temp
             temp += 1
-            res.append(num)
-        s = set(res)
-        for i in res:
-            if c - i in s:
+            if c - num >= 0 and self.isSquare(c - num):
                 return True
         return False
