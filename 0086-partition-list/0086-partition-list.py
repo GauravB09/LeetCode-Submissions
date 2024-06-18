ebb65e7835d4d9000d51acd19b3f8be97fed7faa
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-18 03:06:39 -0700

    Time: 38 ms (51.3%), Space: 16.4 MB (98.73%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,34 +5,49 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        result_node = None
-        result_head = result_node
-        temp_node = None
-        temp_head = temp_node
+        # if not head or not head.next:
+        #     return head
+        # result_node = None
+        # result_head = result_node
+        # temp_node = None
+        # temp_head = temp_node
+        # while head:
+        #     if head.val < x:
+        #         if not result_node:
+        #             result_node = ListNode(head.val)
+        #             result_head = result_node
+        #         else:
+        #             result_node.next = ListNode(head.val)
+        #             result_node.next.next = temp_head
+        #             result_node = result_node.next
+        #     else:
+        #         if not temp_node:
+        #             temp_node = ListNode(head.val)
+        #             temp_head = temp_node
+        #         else:
+        #             temp_node.next = ListNode(head.val)
+        #             temp_node = temp_node.next
+        #     # print(f"Head: {head}\nResult Head: {result_head}\nTemp Head: {temp_head}\nTemp Node: {temp_node}")
+        #     head = head.next
+        # if not result_node:
+        #     return temp_head
+        # if not result_node.next:
+        #     result_node.next = temp_head
+        # return result_head
+        dummy_lesser_head = dummy_lesser = ListNode()
+        dummy_greater_head = dummy_greater = ListNode()
         while head:
+            temp = head.next
             if head.val < x:
-                if not result_node:
-                    result_node = ListNode(head.val)
-                    result_head = result_node
-                else:
-                    result_node.next = ListNode(head.val)
-                    result_node.next.next = temp_head
-                    result_node = result_node.next
+                dummy_lesser.next = head
+                head.next = None
+                dummy_lesser = dummy_lesser.next
             else:
-                if not temp_node:
-                    temp_node = ListNode(head.val)
-                    temp_head = temp_node
-                else:
-                    temp_node.next = ListNode(head.val)
-                    temp_node = temp_node.next
-            # print(f"Head: {head}\nResult Head: {result_head}\nTemp Head: {temp_head}\nTemp Node: {temp_node}")
-            head = head.next
-        if not result_node:
-            return temp_head
-        if not result_node.next:
-            result_node.next = temp_head
-        return result_head
+                dummy_greater.next = head
+                head.next = None
+                dummy_greater = dummy_greater.next
+            head = temp
+        dummy_lesser.next = dummy_greater_head.next
+        return dummy_lesser_head.next
 
 
