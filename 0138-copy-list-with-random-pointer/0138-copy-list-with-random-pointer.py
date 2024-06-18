2eebad9700073ee1f8948b0a62e459b104e3743e
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-18 04:14:29 -0700

    Time: 28 ms (97.22%), Space: 17.3 MB (68.76%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,38 +9,51 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # if not head:
+        #     return None
+        # dic1 = {}
+        # dic2 = {}
+        # dic3 = {}
+        # result_node = result_head = Node(0)
+        # curr = head
+        # arr = []
+        # index = 0
+        # while curr:
+        #     dic3[(curr, curr.val)] = index
+        #     index += 1
+        #     curr = curr.next
+        # curr = head
+        # index = 0
+        # while curr:
+        #     result_node.next = Node(curr.val)
+        #     result_node = result_node.next
+        #     dic2[index] = (result_node, result_node.val)
+        #     if curr.random:
+        #         dic1[index] = dic3[(curr.random, curr.random.val)]
+        #     curr = curr.next
+        #     index += 1
+        # result_node = result_head.next
+        # index = 0
+        # # print(dic1)
+        # # print(dic2)
+        # # print(dic3)
+        # while result_node:
+        #     if index in dic1:
+        #         result_node.random = dic2[dic1[index]][0]
+        #     result_node = result_node.next
+        #     index += 1
+        # return result_head.next
         if not head:
             return None
-        dic1 = {}
-        dic2 = {}
-        dic3 = {}
-        result_node = result_head = Node(0)
+        old_to_new = {}
         curr = head
-        arr = []
-        index = 0
         while curr:
-            dic3[(curr, curr.val)] = index
-            index += 1
+            old_to_new[curr] = Node(curr.val)
             curr = curr.next
         curr = head
-        index = 0
         while curr:
-            result_node.next = Node(curr.val)
-            result_node = result_node.next
-            dic2[index] = (result_node, result_node.val)
-            if curr.random:
-                dic1[index] = dic3[(curr.random, curr.random.val)]
+            old_to_new[curr].next = old_to_new.get(curr.next)
+            old_to_new[curr].random = old_to_new.get(curr.random)
             curr = curr.next
-            index += 1
-        result_node = result_head.next
-        index = 0
-        # print(dic1)
-        # print(dic2)
-        # print(dic3)
-        while result_node:
-            if index in dic1:
-                result_node.random = dic2[dic1[index]][0]
-            result_node = result_node.next
-            index += 1
-        return result_head.next
+        return old_to_new[head]
         
