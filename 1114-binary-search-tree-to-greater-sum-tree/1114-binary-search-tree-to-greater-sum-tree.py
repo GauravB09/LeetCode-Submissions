9401b865ec935eea1d3e88b6db1a8ba11978b97e
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-06-25 14:24:43 -0700

    Time: 37 ms (51.76%), Space: 16.4 MB (74.01%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
@@ -5,29 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def getSumGreaterthanRoot(self, sum_greater_than_self: int, root: TreeNode) -> int:
-        # print(root, sum_greater_than_self)
-        if root.left is None and root.right is None:
-            sum_greater_than_self += root.val
-            root.val = sum_greater_than_self
-            # print("Leaf Node", root, sum_greater_than_self)
-            return sum_greater_than_self
-        if root.right:
-            # print(self.getSumGreaterthanRoot(sum_greater_than_self, root.right))
-            sum_greater_than_self = self.getSumGreaterthanRoot(sum_greater_than_self, root.right)
-            # print("Right Node", root, sum_greater_than_self)
-        root.val += sum_greater_than_self
-        sum_greater_than_self = root.val
-        curr_sum = 0
-        if root.left:
-            # print(self.getSumGreaterthanRoot(sum_greater_than_self, root.left))
-            curr_sum = self.getSumGreaterthanRoot(sum_greater_than_self, root.left)
-        #     print("Left Node", root, sum_greater_than_self, curr_sum)
-        # print("Updated Node", root, sum_greater_than_self, curr_sum)
-        return max(sum_greater_than_self, curr_sum)
-
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        if not root or not (root.left or root.right):
-            return root
-        _ = self.getSumGreaterthanRoot(0, root)
+        su = 0
+        def rec(curr: TreeNode):
+            nonlocal su
+            if curr.right:
+                rec(curr.right)
+            su += curr.val
+            curr.val = su
+            if curr.left:
+                rec(curr.left)
+        rec(root)
         return root
