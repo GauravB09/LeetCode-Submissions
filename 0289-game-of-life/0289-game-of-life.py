03d0c751e7484522c0e7bc5830b6bfa573d23251
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-01 10:43:08 -0700

    Time: 39 ms (43.87%), Space: 16.5 MB (36.6%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -3,31 +3,18 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows = len(board)
-        cols = len(board[0])
-        dic = {}
-        for row in range(rows):
-            for col in range(cols):
-                count = 0
-                if row-1 >= 0:
-                    count += board[row-1][col]
-                    if col-1 >= 0:
-                        count += board[row-1][col-1]
-                    if col+1 < cols:
-                        count += board[row-1][col+1]
-                if row+1 < rows:
-                    count += board[row+1][col]
-                    if col-1 >= 0:
-                        count += board[row+1][col-1]
-                    if col+1 < cols:
-                        count += board[row+1][col+1]
-                if col-1 >= 0:
-                    count += board[row][col-1]
-                if col+1 < cols:
-                    count += board[row][col+1]
-                # print(board[row][col], count)
-                dic[(row, col)] = 1 if (board[row][col] == 0 and count == 3) else (0 if (board[row][col] == 1 and (count > 3 or count < 2)) else board[row][col])
-        # print(dic)
-        for row in range(rows):
-            for col in range(cols):
-                board[row][col] = dic[(row, col)]
+        m = len(board)
+        n = len(board[0])
+        for i in range(m):
+            for j in range(n):
+                ones = 0
+                for x in range(max(0, i - 1), min(m, i + 2)):
+                    for y in range(max(0, j - 1), min(n, j + 2)):
+                        ones += board[x][y] & 1
+                if board[i][j] == 1 and (ones == 3 or ones == 4):
+                    board[i][j] |= 0b10
+                if board[i][j] == 0 and ones == 3:
+                    board[i][j] |= 0b10
+        for i in range(m):
+            for j in range(n):
+                board[i][j] >>= 1
