5ccd4478bc271e35686c853491b8f0d3d8858e44
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-09 12:16:36 -0700

    Time: 33 ms (84.71%), Space: 16.5 MB (49.16%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -1,10 +1,7 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        arr = [i+1 for i in range(n)]
-        count = n
-        while count > 1:
-            for i in range(k-1):
-                arr.append(arr.pop(0))
-            arr.pop(0)
-            count -= 1
-        return arr[0]
+        ans = 0
+        for i in range(2, n + 1):
+            ans = (ans + k) % i
+        # add 1 to convert back to 1 indexing
+        return ans + 1
