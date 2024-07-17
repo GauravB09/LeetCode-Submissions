e3eab225203457b050c14622706a418942b70854
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-16 22:34:54 -0700

    Time: 133 ms (5.22%), Space: 17.2 MB (7.26%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
@@ -6,6 +6,80 @@
 #         self.right = right
 
 class Solution:
+    # def delNodes(
+    #     self, root: Optional[TreeNode], to_delete: List[int]
+    # ) -> List[TreeNode]:
+    #     to_delete_set = set(to_delete)
+    #     forest = []
+
+    #     root = self._process_node(root, to_delete_set, forest)
+
+    #     # If the root is not deleted, add it to the forest
+    #     if root:
+    #         forest.append(root)
+
+    #     return forest
+
+    # def _process_node(
+    #     self, node: TreeNode, to_delete_set: Set[int], forest: List[TreeNode]
+    # ) -> TreeNode:
+    #     if not node:
+    #         return None
+
+    #     node.left = self._process_node(node.left, to_delete_set, forest)
+    #     node.right = self._process_node(node.right, to_delete_set, forest)
+
+    #     # Node Evaluation: Check if the current node needs to be deleted
+    #     if node.val in to_delete_set:
+    #         # If the node has left or right children, add them to the forest
+    #         if node.left:
+    #             forest.append(node.left)
+    #         if node.right:
+    #             forest.append(node.right)
+    #         # Delete the current node by returning None to its parent
+    #         return None
+
+    #     return node
+
+    # def delNodes(
+    #     self, root: Optional[TreeNode], to_delete: List[int]
+    # ) -> List[TreeNode]:
+    #     if not root:
+    #         return []
+
+    #     to_delete_set = set(to_delete)
+    #     forest = []
+
+    #     nodes_queue = deque([root])
+
+    #     while nodes_queue:
+    #         current_node = nodes_queue.popleft()
+
+    #         if current_node.left:
+    #             nodes_queue.append(current_node.left)
+    #             # Disconnect the left child if it needs to be deleted
+    #             if current_node.left.val in to_delete_set:
+    #                 current_node.left = None
+
+    #         if current_node.right:
+    #             nodes_queue.append(current_node.right)
+    #             # Disconnect the right child if it needs to be deleted
+    #             if current_node.right.val in to_delete_set:
+    #                 current_node.right = None
+
+    #         # If the current node needs to be deleted, add its non-null children to the forest
+    #         if current_node.val in to_delete_set:
+    #             if current_node.left:
+    #                 forest.append(current_node.left)
+    #             if current_node.right:
+    #                 forest.append(current_node.right)
+
+    #     # Ensure the root is added to the forest if it is not to be deleted
+    #     if root.val not in to_delete_set:
+    #         forest.append(root)
+
+    #     return forest
+
     def findParentNode(self, root: Optional[TreeNode], node_val: int) -> (Optional[TreeNode], str):
         if not root:
             return (None, "")
