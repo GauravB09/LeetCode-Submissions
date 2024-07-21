078d6532a3c54925e9b03a5b0c84153bf3dabbf5
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-20 19:55:19 -0700

    Time: 369 ms (90.63%), Space: 21.8 MB (52.78%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.py
@@ -17,15 +17,35 @@ class Solution:
         #         curr_col_sum[j] += orig_matrix[i][j]
 
         # return orig_matrix
+        
+        # N = len(rowSum)
+        # M = len(colSum)
+
+        # orig_matrix = [[0] * M for _ in range(N)]
+        # for i in range(N):
+        #     for j in range(M):
+        #         orig_matrix[i][j] = min(rowSum[i], colSum[j])
+
+        #         rowSum[i] -= orig_matrix[i][j]
+        #         colSum[j] -= orig_matrix[i][j]
+
+        # return orig_matrix
+
         N = len(rowSum)
         M = len(colSum)
 
         orig_matrix = [[0] * M for _ in range(N)]
-        for i in range(N):
-            for j in range(M):
-                orig_matrix[i][j] = min(rowSum[i], colSum[j])
+        i, j = 0, 0
 
-                rowSum[i] -= orig_matrix[i][j]
-                colSum[j] -= orig_matrix[i][j]
+        while i < N and j < M:
+            orig_matrix[i][j] = min(rowSum[i], colSum[j])
+
+            rowSum[i] -= orig_matrix[i][j]
+            colSum[j] -= orig_matrix[i][j]
+
+            if rowSum[i] == 0:
+                i += 1
+            else:
+                j += 1
 
         return orig_matrix
