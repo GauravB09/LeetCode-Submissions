4a76ea8b5669ddeb64aad6a516bb44e1d0e2fdee
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-23 02:48:25 -0700

    Time: 602 ms (10.81%), Space: 25.5 MB (56.71%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.py
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.py
@@ -1,40 +1,38 @@
 class Solution:
-    def buildMatrix(
-        self,
-        k: int,
-        row_conditions: List[List[int]],
-        col_conditions: List[List[int]],
-    ) -> List[List[int]]:
-        order_rows = self.__topo_sort(row_conditions, k)
-        order_columns = self.__topo_sort(col_conditions, k)
-        if not order_rows or not order_columns:
-            return []
-        matrix = [[0] * k for _ in range(k)]
-        for i in range(k):
-            for j in range(k):
-                if order_rows[i] == order_columns[j]:
-                    matrix[i][j] = order_rows[i]
-        return matrix
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        def get_indices(conditions, arr):
+            indegree = [0] * (k + 1)
+            graph = defaultdict(list)
 
-    def __topo_sort(self, edges, n):
-        adj = [[] for _ in range(n + 1)]
-        deg = [0] * (n + 1)
-        order = []
-        for x in edges:
-            adj[x[0]].append(x[1])
-            deg[x[1]] += 1
-        q = deque()
-        for i in range(1, n + 1):
-            if deg[i] == 0:
-                q.append(i)
-        while q:
-            f = q.popleft()
-            order.append(f)
-            n -= 1
-            for v in adj[f]:
-                deg[v] -= 1
-                if deg[v] == 0:
-                    q.append(v)
-        if n != 0:
+            for u, v in conditions:
+                indegree[v] += 1
+                graph[u].append(v)
+            
+            queue = deque()
+            for i in range(1, k + 1):
+                if indegree[i] == 0:
+                    queue.append(i)
+            
+            i = 0
+            while queue: 
+                u = queue.popleft()
+                arr[u] = i
+                i += 1
+                for v in graph[u]:
+                    indegree[v] -= 1
+                    if indegree[v] == 0:
+                        queue.append(v)
+            if i != k:
+                return []
+            return arr
+    
+        rows = get_indices(rowConditions, [0] * (k + 1))
+        cols = get_indices(colConditions, [0] * (k + 1))
+
+        if not rows or not cols:
             return []
-        return order
+
+        matrix = [[0] * k for _ in range(k)]
+        for i in range(1, k+1):
+            matrix[rows[i]][cols[i]] = i
+        return matrix
