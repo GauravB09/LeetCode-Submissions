e56d7d29960e754d977cd887888b9a8efa67435a
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-23 19:02:36 -0700

    Time: 789 ms (97.28%), Space: 23 MB (76.63%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -1,9 +1,23 @@
 class Solution:
-    def getNewNumber(self, mapping: List[int], num: str) -> int:
-        ans = ""
-        for i in num:
-            ans += str(mapping[int(i)])
-        return int(ans)
+    # def getNewNumber(self, mapping: List[int], num: str) -> int:
+    #     ans = ""
+    #     for i in num:
+    #         ans += str(mapping[int(i)])
+    #     return int(ans)
+
+    # def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+    #     return sorted(nums, key=(lambda x : self.getNewNumber(mapping, str(x))))
+
+    def getNewNumber(self, mapping: List[int], num: int) -> int:
+        ans = 0
+        place = 1
+        if num == 0:
+            return mapping[0]
+        while num > 0:
+            ans = (place * mapping[num%10]) + ans
+            place *= 10
+            num //= 10
+        return ans
 
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        return sorted(nums, key=(lambda x : self.getNewNumber(mapping, str(x))))
+        return sorted(nums, key=(lambda x : self.getNewNumber(mapping, x)))
