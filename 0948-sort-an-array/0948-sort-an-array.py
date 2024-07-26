6c3cdc66601c85588809ec01f13a28be37c7d6cc
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-25 18:53:11 -0700

    Time: 1119 ms (54.59%), Space: 25.6 MB (27.22%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,7 +1,38 @@
 import heapq
 class Solution:
+    def merge(self, arr1: List[int], arr2: List[int], n1: int, n2: int) -> List[int]:
+        ans = [0 for _ in range(n1 + n2)]
+        i = 0
+        j = 0
+        k = 0
+        while i < n1 and j < n2:
+            if arr1[i] <= arr2[j]:
+                ans[k] = arr1[i]
+                i += 1
+            else:
+                ans[k] = arr2[j]
+                j += 1
+            k += 1
+        while i < n1:
+            ans[k] = arr1[i]
+            i += 1
+            k += 1
+        while j < n2:
+            ans[k] = arr2[j]
+            j += 1
+            k += 1
+        return ans
+
+    def mergeSort(self, arr: List[int], n: int) -> List[int]:
+        if n == 1:
+            return arr
+        elif n == 2:
+            return [arr[0], arr[1]] if arr[0] <= arr[1] else [arr[1], arr[0]]
+        else:
+            mid = n // 2
+            arr1 = self.mergeSort(arr[:mid], mid)
+            arr2 = self.mergeSort(arr[mid:], n - mid)
+            return self.merge(arr1, arr2, mid, n - mid)
+
     def sortArray(self, nums: List[int]) -> List[int]:
-        heapq.heapify(nums)
-        n = len(nums)
-        arr = [heapq.heappop(nums) for _ in range(n)]
-        return arr
+        return self.mergeSort(nums, len(nums))
