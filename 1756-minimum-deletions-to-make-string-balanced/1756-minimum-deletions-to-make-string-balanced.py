fdb18edb8f84ae3fb93d422fc587683d6d1892cc
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-07-30 16:16:25 -0700

    Time: 569 ms (27.54%), Space: 20.3 MB (20.33%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.py
@@ -1,19 +1,35 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
+        # n = len(s)
+        # count_a = [0] * n
+        # count_b = [0] * n
+        # b_count = 0
+
+        # # First pass: compute count_b which stores the number of
+        # # 'b' characters to the left of the current position.
+        # for i in range(n):
+        #     count_b[i] = b_count
+        #     if s[i] == "b":
+        #         b_count += 1
+
+        # a_count = 0
+        # # Second pass: compute count_a which stores the number of
+        # # 'a' characters to the right of the current position
+        # for i in range(n - 1, -1, -1):
+        #     count_a[i] = a_count
+        #     if s[i] == "a":
+        #         a_count += 1
+
+        # min_deletions = n
+        # # Third pass: iterate through the string to find the minimum deletions
+        # for i in range(n):
+        #     min_deletions = min(min_deletions, count_a[i] + count_b[i])
+        # return min_deletions
         n = len(s)
         count_a = [0] * n
-        count_b = [0] * n
-        b_count = 0
-
-        # First pass: compute count_b which stores the number of
-        # 'b' characters to the left of the current position.
-        for i in range(n):
-            count_b[i] = b_count
-            if s[i] == "b":
-                b_count += 1
-
         a_count = 0
-        # Second pass: compute count_a which stores the number of
+
+        # First pass: compute count_a which stores the number of
         # 'a' characters to the right of the current position
         for i in range(n - 1, -1, -1):
             count_a[i] = a_count
@@ -21,7 +37,11 @@ class Solution:
                 a_count += 1
 
         min_deletions = n
-        # Third pass: iterate through the string to find the minimum deletions
+        b_count = 0
+        # Second pass: compute minimum deletions on the fly
         for i in range(n):
-            min_deletions = min(min_deletions, count_a[i] + count_b[i])
+            min_deletions = min(count_a[i] + b_count, min_deletions)
+            if s[i] == "b":
+                b_count += 1
+
         return min_deletions
