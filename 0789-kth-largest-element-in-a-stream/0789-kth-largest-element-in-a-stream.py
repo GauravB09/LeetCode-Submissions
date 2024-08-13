fe4172b50306dc7158819f44ce9b51d55db3a13e
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2024-08-12 20:36:47 -0700

    Time: 79 ms (59.05%), Space: 20.7 MB (42%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,27 +1,43 @@
 class KthLargest:
 
+    # def __init__(self, k: int, nums: List[int]):
+    #     self.k = k
+    #     self.stream = nums
+    #     self.stream.sort()
+
+    # def add(self, val: int) -> int:
+    #     index = self.getIndex(val)
+    #     # Add val to correct position
+    #     self.stream.insert(index, val)
+    #     return self.stream[-self.k]
+
+    # def getIndex(self, val: int) -> int:
+    #     left, right = 0, len(self.stream) - 1
+    #     while left <= right:
+    #         mid = (left + right) // 2
+    #         mid_element = self.stream[mid]
+    #         if mid_element == val:
+    #             return mid
+    #         # Go to left half
+    #         elif mid_element > val:
+    #             right = mid - 1
+    #         # Go to right half
+    #         else:
+    #             left = mid + 1
+    #     return left
+
     def __init__(self, k: int, nums: List[int]):
+        self.min_heap = []
         self.k = k
-        self.stream = nums
-        self.stream.sort()
+
+        for num in nums:
+            self.add(num)
 
     def add(self, val: int) -> int:
-        index = self.getIndex(val)
-        # Add val to correct position
-        self.stream.insert(index, val)
-        return self.stream[-self.k]
-
-    def getIndex(self, val: int) -> int:
-        left, right = 0, len(self.stream) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            mid_element = self.stream[mid]
-            if mid_element == val:
-                return mid
-            # Go to left half
-            elif mid_element > val:
-                right = mid - 1
-            # Go to right half
-            else:
-                left = mid + 1
-        return left
+        # Add to our min_heap if we haven't processed k elements yet
+        # or if val is greater than the top element (the k-th largest)
+        if len(self.min_heap) < self.k or self.min_heap[0] < val:
+            heapq.heappush(self.min_heap, val)
+            if len(self.min_heap) > self.k:
+                heapq.heappop(self.min_heap)
+        return self.min_heap[0]
