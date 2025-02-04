23a38cb45ddf223d8f9674b86478293b5eadbe7a
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2025-02-03 21:56:45 -0800

    Time: 41 ms (33.97%), Space: 17.7 MB (40.69%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -3,9 +3,9 @@ class Solution:
         n1 = len(word1)
         n2 = len(word2)
         s = ""
-        for i in range(min(n1, n2)):
-            s += word1[i]
-            s += word2[i]
-        s += word1[i+1:]
-        s += word2[i+1:]
+        for i in range(max(n1, n2)):
+            if i < n1:
+                s += word1[i]
+            if i < n2:
+                s += word2[i]
         return s
