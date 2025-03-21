80970323bbc2e2251a2b4f915feae6bb0998dc3c
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2025-03-20 23:41:48 -0700

    Time: 25 ms (35.26%), Space: 19.4 MB (6.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        dic = {}
+        nums_1 = [[nums[i], i] for i in range(n)]
+        nums_1.sort()
+
+        def bin_search(remainder, left, right):
+            if left <= right:
+                mid = (left + right)//2
+                if nums_1[mid][0] == remainder:
+                    return nums_1[mid][1]
+                elif nums_1[mid][0] < remainder:
+                    return bin_search(remainder, mid+1, right)
+                else:
+                    return bin_search(remainder, left, mid-1)
+            return -1
+        
         for i in range(n):
-            if (target-nums[i]) in dic:
-                return [dic[target - nums[i]], i]
-            dic[nums[i]] = i
+            remainder_index = bin_search(target-nums_1[i][0], i+1, n-1)
+            if remainder_index != -1:
+                return [nums_1[i][1],remainder_index]
