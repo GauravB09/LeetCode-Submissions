aab83f1eb330da361eb8b791c0c7aece39c9c7b8
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2025-03-24 13:33:45 -0700

    Time: 4 ms (35.78%), Space: 18.2 MB (20.67%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,9 +1,39 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1.extend(nums2)
-        nums1.sort()
-        n = len(nums1)
+        # nums1.extend(nums2)
+        # nums1.sort()
+        # n = len(nums1)
+        # if n%2==1:
+        #     return float(nums1[n//2])
+        # else:
+        #     return float(nums1[n//2] + nums1[n//2-1])/2 
+        n1 = len(nums1)
+        n2 = len(nums2)
+        n = n1+n2
+        new_arr = [0 for _ in range(n)]
+        i = 0
+        j = 0
+        k = 0
+        while i < n1 and j < n2 and k < n:
+            if nums1[i] < nums2[j]:
+                new_arr[k] = nums1[i]
+                i += 1
+            else:
+                new_arr[k] = nums2[j]
+                j += 1
+            k += 1
+        # print(i, j, k, new_arr)
+        while i < n1 and k < n:
+            new_arr[k] = nums1[i]
+            i += 1
+            k += 1
+        # print(i, j, k, new_arr)
+        while j < n2 and k < n:
+            new_arr[k] = nums2[j]
+            j += 1
+            k += 1
+        # print(new_arr)
         if n%2==1:
-            return float(nums1[n//2])
+            return float(new_arr[n//2])
         else:
-            return float(nums1[n//2] + nums1[n//2-1])/2 
+            return float(new_arr[n//2] + new_arr[n//2-1])/2 
