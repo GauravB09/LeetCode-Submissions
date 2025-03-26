0ccfc26a90e762c762aa223819d09ef538669e95
Gaurav Baisware <74409315+GauravB09@users.noreply.github.com>
2025-03-26 15:32:39 -0700

    Time: 228 ms (5.04%), Space: 75.4 MB (31.96%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+class TimeMap:
+    def binarySearch(self, arr, target):
+        # print("Bin", arr, target)
+        low = 0
+        high = len(arr) - 1
+        ans = 0
+        while low <= high:
+            mid = (low + high)//2
+            # print("Bin1", low, high, mid, arr[mid])
+            # if arr[mid][0] == target:
+            #     return mid
+            if arr[mid][0] <= target:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        curr_key_list = self.time_map[key]
+        if curr_key_list == []:
+            self.time_map[key] = [[0, ""], [timestamp, value]]
+        else:
+            correct_index = self.binarySearch(curr_key_list, timestamp)
+            if curr_key_list[correct_index][0] == timestamp:
+                curr_key_list[correct_index][1] = value
+            else:
+                curr_key_list.insert(correct_index+1, [timestamp, value])
+            self.time_map[key] = curr_key_list
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.time_map:
+            return ""
+        curr_key_list = self.time_map[key]
+        correct_index = self.binarySearch(curr_key_list, timestamp)
+        # print(timestamp, curr_key_list, correct_index)
+        return curr_key_list[correct_index][1]
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
